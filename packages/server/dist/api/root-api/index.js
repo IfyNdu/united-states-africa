98f70c937e0c97f6d2577315109c69eb72df3d5b
Ify Ndu <ify@Ifys-MacBook-Pro.local>
2019-12-13 00:54:37 +0000

    fixed untracked files

--- packages/server/dist/api/root-api/index.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.default = (router) => {
-    router.get('/', (req, res, next) => {
-        res.send({ expense: 'EXPENSE!!!' });
-    });
-    return router;
-};
-//# sourceMappingURL=index.js.map
--- packages/server/dist/db/index.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-const sequelize_typescript_1 = require("sequelize-typescript");
-exports.default = {
-    init: (url) => {
-        return new sequelize_typescript_1.Sequelize(url, { models: ['./models'] });
-    }
-};
-//# sourceMappingURL=index.js.map
